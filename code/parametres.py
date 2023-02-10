0cfb66cab1acf4845e58ec2a1462b0eaf00a4249
Louis Declerck <84902823+Louis2675@users.noreply.github.com>
2023-02-10 19:37:49 +0000

    Fonction choisir_case fonctionnelle

--- code/parametres.py
+++ code/parametres.py
@@ -1,20 +1,40 @@
-SYMBOLE_JOUEUR_1 = "⦿" # Initialise le suymbole du joueur 1
-SYMBOLE_JOUEUR_2 = "⦾" # Initialise le suymbole du joueur 2
-SYMBOLE_VIDE = "." # Initialise le suymbole d'une case vide
+"""Ce fichier contient les paramètres du jeu"""
 
-grille = [[], [], [], []]  # Initialise la grille
-sous_grille = [[[],[],[]],[[],[],[]],[[],[],[]]] # Grille d'un seul morpion
+SYMBOLE_JOUEUR_1 = "⦿"  # Initialise le suymbole du joueur 1
+SYMBOLE_JOUEUR_2 = "⦾"  # Initialise le suymbole du joueur 2
+SYMBOLE_VIDE = "."  # Initialise le suymbole d'une case vide
 
-def copie_liste(liste):
-    """Copie une liste"""
-    copie = []
-    for element in liste:
-        copie.append(element)
-    return copie
+grille = [
+    [
+        [[], [] ,[]], [[], [], []], [[], [], []]
+    ],
+    [
+        [[], [] ,[]], [[], [], []], [[], [], []]
+    ],
+    [
+        [[], [] ,[]], [[], [], []], [[], [], []]
+    ],
+    [
+        [[], [] ,[]], [[], [], []], [[], [], []]
+    ]
+]
 
-# Mettre sous grille dans chaque case de grille
+for i in range(len(grille)):
+    for j in range(len(grille[i])):
+        for k in range(len(grille[i][j])):
+            grille[i][j][k] = SYMBOLE_VIDE
 
-for i in enumerate(grille): # Pour chaque case de grille
-    grille[i[0]] = copie_liste(sous_grille) # Copie la sous grille dans chaque case de grille
 
-print(grille)
+def choisir_case(grille, symbole):
+    """Permet de choisir une case pour y placer un symbole"""
+    cpt = 0
+    while cpt == 0:
+        case = int(input("Sélectionnez votre case : ")) -1
+        lig = int(input("Sélectionnez votre ligne : ")) -1
+        col = int(input("Sélectionnez votre colonne : ")) -1
+        if grille[case][lig][col] == SYMBOLE_VIDE:
+            print(symbole)
+            grille[case][lig][col] = symbole
+            cpt = 1
+        else: print(False)
+    return grille
