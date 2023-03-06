baf153c21d929aae98b65178d2638561f60c64da
Louis2675 <2eclerck.louis@gmail.com>
2023-03-06 22:59:27 +0100

    Nouvelle update 22:59

---
+++ code/tour_de_jeu.py
@@ -0,0 +1,23 @@
+def coor(liste_info, taille_grande_grille):
+        if liste_info[1] % taille_grande_grille == 0: # si la case est un multiple de la taille de la grille
+            grande_ligne = liste_info[1] // taille_grande_grille - 1 # on recupere la ligne de la grande grille
+            grande_colonne = taille_grande_grille -1 
+        
+        else: 
+            grande_ligne = liste_info[1] // taille_grande_grille # on recupere la ligne de la grande grille
+            grande_colonne = liste_info[1] % taille_grande_grille - 1 # on recupere la colonne de la grande grille
+        return grande_ligne, grande_colonne
+    
+print (coor((0, 6), 3))
+
+# [][][] [][][] [][][]
+# [][][] [][][] [][][]
+# [][][] [][][] [][][]
+
+# [][][] [][][] [][][]
+# [][][] [][][] [][][]
+# [][][] [][][] [][][]
+
+# [][][] [][][] [][][]
+# [][][] [][][] [][][]
+# [][][] [][][] [][][]
