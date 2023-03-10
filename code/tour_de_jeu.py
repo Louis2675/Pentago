949e0924b43c2b7cedd2ae6d84b81893a4b1e525
Louis Declerck <2eclerck.louis@gmail.com>
2023-03-10 10:51:25 +0100

    wsh

--- code/tour_de_jeu.py
+++ code/tour_de_jeu.py
@@ -1,23 +1,10 @@
-def coor(liste_info, taille_grande_grille):
-        if liste_info[1] % taille_grande_grille == 0: # si la case est un multiple de la taille de la grille
-            grande_ligne = liste_info[1] // taille_grande_grille - 1 # on recupere la ligne de la grande grille
-            grande_colonne = taille_grande_grille -1 
-        
-        else: 
-            grande_ligne = liste_info[1] // taille_grande_grille # on recupere la ligne de la grande grille
-            grande_colonne = liste_info[1] % taille_grande_grille - 1 # on recupere la colonne de la grande grille
-        return grande_ligne, grande_colonne
-    
-print (coor((0, 6), 3))
+from parametres import initialisation_grille, SYMBOLE_JOUEUR_1, SYMBOLE_JOUEUR_2
 
-# [][][] [][][] [][][]
-# [][][] [][][] [][][]
-# [][][] [][][] [][][]
 
-# [][][] [][][] [][][]
-# [][][] [][][] [][][]
-# [][][] [][][] [][][]
+grille_modifiee = [
+    [SYMBOLE_JOUEUR_1,2,SYMBOLE_JOUEUR_1],
+    [3,SYMBOLE_JOUEUR_1,5],
+    [SYMBOLE_JOUEUR_1,6,SYMBOLE_JOUEUR_1]
+    ]
 
-# [][][] [][][] [][][]
-# [][][] [][][] [][][]
-# [][][] [][][] [][][]
+print(alignement_diagonnal(grille_modifiee, 3))
